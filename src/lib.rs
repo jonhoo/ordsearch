7289b30ae5216e2bf55a0f73271e535ba2e1fcf2
Jon Gjengset <jon@thesquareplanet.com>
2018-06-29 11:49:59 -0400

    Reference new results.
    Fixes #3 and continues #2.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//! > NOTE: This crate is currently *slower* than using `Vec::binary_search` over a pre-sorted
+//! > NOTE: This crate is generally *slower* than using `Vec::binary_search` over a pre-sorted
 //! > vector, contrary to the claims in the referenced paper, and is mainly presented for
 //! > curiosity's sake at this point.
 //!
@@ -37,58 +37,69 @@
 //!
 //! This will benchmark both construction and search with different number of values, and
 //! differently sized values -- look for the line that aligns closest with your data. The general
-//! trend is that `ordsearch` is faster when `n` is smaller and `T` is larger. You may also want to
-//! compare with the pending Rust PR "[Improve SliceExt::binary_search
-//! performance](https://github.com/rust-lang/rust/pull/45333)".
-//! [Summarized](https://github.com/BurntSushi/cargo-benchcmp) results from my laptop (an X1 Carbon
-//! with i7-5600U @ 2.60GHz) are given below.
+//! trend is that `ordsearch` is faster when `n` is smaller and `T` is larger as long as you
+//! compile with
+//! [`target-cpu=native`](https://github.com/jonhoo/ordsearch/issues/2#issuecomment-390441137) and
+//! [`lto=thin`](https://github.com/jonhoo/ordsearch/issues/2#issuecomment-390446671). The
+//! performance gain seems to be best on Intel processors, and is smaller since the (relatively)
+//! recent improvement to [SliceExt::binary_search
+//! performance](https://github.com/rust-lang/rust/pull/45333).
+//!
+//! Below are [summarized](https://github.com/BurntSushi/cargo-benchcmp) results from an AMD
+//! ThreadRipper 2600X CPU run with:
+//!
+//! ```console
+//! $ rustc +nightly --version
+//! rustc 1.28.0-nightly (e3bf634e0 2018-06-28)
+//! $ env CARGO_INCREMENTAL=0 RUSTFLAGS='-C target-cpu=native -C lto=thin' cargo +nightly bench --features nightly
+//! ```
 //!
 //! Compared to binary search over a sorted vector:
 //!
 //! ```diff,ignore
 //!  name           sorted_vec ns/iter  this ns/iter  diff ns/iter   diff %  speedup
-//! -u32::l1        51                  103                     52  101.96%   x 0.50
-//! -u32::l1_dup    42                  90                      48  114.29%   x 0.47
-//! -u32::l2        67                  150                     83  123.88%   x 0.45
-//! -u32::l2_dup    66                  146                     80  121.21%   x 0.45
-//! -u32::l3        118                 352                    234  198.31%   x 0.34
-//! -u32::l3_dup    119                 352                    233  195.80%   x 0.34
-//! -u8::l1         47                  97                      50  106.38%   x 0.48
-//! -u8::l1_dup     36                  85                      49  136.11%   x 0.42
-//! -u8::l2         56                  149                     93  166.07%   x 0.38
-//! -u8::l2_dup     45                  141                     96  213.33%   x 0.32
-//! -u8::l3         68                  224                    156  229.41%   x 0.30
-//! -u8::l3_dup     52                  197                    145  278.85%   x 0.26
-//! -usize::l1      51                  105                     54  105.88%   x 0.49
-//! -usize::l1_dup  42                  91                      49  116.67%   x 0.46
-//! -usize::l2      68                  153                     85  125.00%   x 0.44
-//! -usize::l2_dup  67                  148                     81  120.90%   x 0.45
-//! -usize::l3      139                 463                    324  233.09%   x 0.30
-//! -usize::l3_dup  139                 467                    328  235.97%   x 0.30
+//! -u32::l1        49                  54                       5   10.20%   x 0.91
+//! +u32::l1_dup    40                  35                      -5  -12.50%   x 1.14
+//! -u32::l2        63                  72                       9   14.29%   x 0.88
+//! +u32::l2_dup    64                  62                      -2   -3.12%   x 1.03
+//! -u32::l3        120                 273                    153  127.50%   x 0.44
+//! -u32::l3_dup    117                 219                    102   87.18%   x 0.53
+//! +u8::l1         42                  37                      -5  -11.90%   x 1.14
+//! +u8::l1_dup     29                  28                      -1   -3.45%   x 1.04
+//! +u8::l2         43                  49                       6   13.95%   x 0.88
+//! -u8::l2_dup     33                  35                       2    6.06%   x 0.94
+//! -u8::l3         45                  66                      21   46.67%   x 0.68
+//! -u8::l3_dup     35                  51                      16   45.71%   x 0.69
+//! -usize::l1      49                  54                       5   10.20%   x 0.91
+//! +usize::l1_dup  38                  37                      -1   -2.63%   x 1.03
+//! -usize::l2      65                  76                      11   16.92%   x 0.86
+//! +usize::l2_dup  65                  64                      -1   -1.54%   x 1.02
+//! -usize::l3      141                 303                    162  114.89%   x 0.47
+//! -usize::l3_dup  140                 274                    134   95.71%   x 0.51
 //! ```
 //!
 //! Compared to a `BTreeSet`:
 //!
 //! ```diff,ignore
 //!  name           btreeset ns/iter  this ns/iter  diff ns/iter   diff %  speedup
-//! +u32::l1        294               103                   -191  -64.97%   x 2.85
-//! +u32::l1_dup    169               90                     -79  -46.75%   x 1.88
-//! +u32::l2        364               150                   -214  -58.79%   x 2.43
-//! +u32::l2_dup    239               146                    -93  -38.91%   x 1.64
-//! +u32::l3        723               352                   -371  -51.31%   x 2.05
-//! +u32::l3_dup    454               352                   -102  -22.47%   x 1.29
-//! +u8::l1         222               97                    -125  -56.31%   x 2.29
-//! +u8::l1_dup     155               85                     -70  -45.16%   x 1.82
-//! +u8::l2         222               149                    -73  -32.88%   x 1.49
-//! +u8::l2_dup     155               141                    -14   -9.03%   x 1.10
-//!  u8::l3         222               224                      2    0.90%   x 0.99
-//! -u8::l3_dup     155               197                     42   27.10%   x 0.79
-//! +usize::l1      298               105                   -193  -64.77%   x 2.84
-//! +usize::l1_dup  168               91                     -77  -45.83%   x 1.85
-//! +usize::l2      368               153                   -215  -58.42%   x 2.41
-//! +usize::l2_dup  242               148                    -94  -38.84%   x 1.64
-//! +usize::l3      780               463                   -317  -40.64%   x 1.68
-//! +usize::l3_dup  495               467                    -28   -5.66%   x 1.06
+//! +u32::l1        68                54                     -14  -20.59%   x 1.26
+//! +u32::l1_dup    45                35                     -10  -22.22%   x 1.29
+//! +u32::l2        88                72                     -16  -18.18%   x 1.22
+//! -u32::l2_dup    61                62                       1    1.64%   x 0.98
+//! +u32::l3        346               273                    -73  -21.10%   x 1.27
+//! -u32::l3_dup    136               219                     83   61.03%   x 0.62
+//! +u8::l1         45                37                      -8  -17.78%   x 1.22
+//! +u8::l1_dup     31                28                      -3   -9.68%   x 1.11
+//! -u8::l2         44                49                       5   11.36%   x 0.90
+//! -u8::l2_dup     31                35                       4   12.90%   x 0.89
+//! -u8::l3         43                66                      23   53.49%   x 0.65
+//! -u8::l3_dup     30                51                      21   70.00%   x 0.59
+//! +usize::l1      67                54                     -13  -19.40%   x 1.24
+//! +usize::l1_dup  44                37                      -7  -15.91%   x 1.19
+//! +usize::l2      89                76                     -13  -14.61%   x 1.17
+//! -usize::l2_dup  60                64                       4    6.67%   x 0.94
+//! +usize::l3      393               303                    -90  -22.90%   x 1.30
+//! -usize::l3_dup  163               274                    111   68.10%   x 0.59
 //! ```
 //!
 //! # Future work
@@ -447,8 +458,8 @@ mod tests {
 mod b {
     use super::OrderedCollection;
     use std::collections::BTreeSet;
-    use test::Bencher;
     use test::black_box;
+    use test::Bencher;
 
     // these benchmarks borrow from https://github.com/rust-lang/rust/pull/45333
 
