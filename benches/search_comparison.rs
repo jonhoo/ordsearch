c2896d8087947604c2abd966069f903c38683f0a
qqwy <qqwy@gmx.com>
2023-08-25 21:20:11 +0200

    Make cargo fmt happy ;-)

--- benches/search_comparison.rs
+++ benches/search_comparison.rs
@@ -3,8 +3,8 @@ extern crate num_traits;
 extern crate ordsearch;
 
 use criterion::{
-    black_box, criterion_group, criterion_main, measurement::WallTime, BenchmarkGroup, BenchmarkId,
-    Criterion, PlotConfiguration, AxisScale,
+    black_box, criterion_group, criterion_main, measurement::WallTime, AxisScale, BenchmarkGroup,
+    BenchmarkId, Criterion, PlotConfiguration,
 };
 use ordsearch::OrderedCollection;
 use std::{collections::BTreeSet, convert::TryFrom, time::Duration};
@@ -32,8 +32,7 @@ where
         + Clone,
     <T as TryFrom<usize>>::Error: core::fmt::Debug,
 {
-    let plot_config = PlotConfiguration::default()
-        .summary_scale(AxisScale::Logarithmic);
+    let plot_config = PlotConfiguration::default().summary_scale(AxisScale::Logarithmic);
 
     let sizes = [
         8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4069, 8192, 16384, 32768, 65536,
@@ -42,7 +41,10 @@ where
     {
         let groupname = format!("Search {}", std::any::type_name::<T>());
         let mut group = c.benchmark_group(groupname);
-        group.warm_up_time(WARM_UP_TIME).measurement_time(MEASUREMENT_TIME).plot_config(plot_config.clone());
+        group
+            .warm_up_time(WARM_UP_TIME)
+            .measurement_time(MEASUREMENT_TIME)
+            .plot_config(plot_config.clone());
 
         for i in sizes.iter() {
             search_bench_case::<MAX, T, _>(
@@ -76,7 +78,10 @@ where
     {
         let groupname = format!("Search (with duplicates) {}", std::any::type_name::<T>());
         let mut group = c.benchmark_group(groupname);
-        group.warm_up_time(WARM_UP_TIME).measurement_time(MEASUREMENT_TIME).plot_config(plot_config.clone());
+        group
+            .warm_up_time(WARM_UP_TIME)
+            .measurement_time(MEASUREMENT_TIME)
+            .plot_config(plot_config.clone());
 
         for i in sizes.iter() {
             search_bench_case::<MAX, T, _>(
@@ -110,7 +115,10 @@ where
     {
         let groupname = format!("Construction {}", std::any::type_name::<T>());
         let mut group = c.benchmark_group(groupname);
-        group.warm_up_time(WARM_UP_TIME).measurement_time(MEASUREMENT_TIME).plot_config(plot_config.clone());
+        group
+            .warm_up_time(WARM_UP_TIME)
+            .measurement_time(MEASUREMENT_TIME)
+            .plot_config(plot_config.clone());
 
         for i in sizes.iter() {
             construction_bench_case::<MAX, T, _>(
@@ -132,7 +140,10 @@ where
             std::any::type_name::<T>()
         );
         let mut group = c.benchmark_group(groupname);
-        group.warm_up_time(WARM_UP_TIME).measurement_time(MEASUREMENT_TIME).plot_config(plot_config);
+        group
+            .warm_up_time(WARM_UP_TIME)
+            .measurement_time(MEASUREMENT_TIME)
+            .plot_config(plot_config);
 
         for i in sizes.iter() {
             construction_bench_case::<MAX, T, _>(
