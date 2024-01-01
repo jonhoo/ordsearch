95c9fb167010762b47305d719f81fb47c70f6fdf
Denis Bazhenov <dotsid@gmail.com>
2024-01-01 17:20:52 +0700

    Added benchmarks of larger sizes

--- benches/search_comparison.rs
+++ benches/search_comparison.rs
@@ -38,7 +38,8 @@ where
     let plot_config = PlotConfiguration::default().summary_scale(AxisScale::Logarithmic);
 
     let sizes = [
-        8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4069, 8192, 16384, 32768, 65536,
+        8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4069, 8192, 16384, 32768, 65536, 1048576,
+        10485760,
     ];
 
     {
